043b94e4de5195c248ad63943d30d969538dc9c6
KJMAN678 <kj.fireman5@gmail.com>
2025-07-07 21:40:11 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+### PlayCanvas
+
+- [PlayCanvasホスティング](https://developer.playcanvas.com/ja/user-manual/publishing/web/playcanvas-hosting/)
+- [セルフホスティング](https://developer.playcanvas.com/ja/user-manual/publishing/web/self-hosting/#iframe-embedding)
+
+```sh
+$ npm create vite@latest download -- --template react-ts
+$ cd download
+$ npm install
+```
+
+- ダウンロードしたファイルをdownload ディレクトリ直下にコピーする
+
+```sh
+$ npm run dev
+```
+http://localhost:5173/
---
+++ download/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2275171.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 239461808, 239461809, 239461807 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/239461826/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/239461830/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/239461821/1/ammo.js', 'preload' : true},
+];
